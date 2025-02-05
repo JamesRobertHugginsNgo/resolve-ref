2abe5cc93ee43049a237b146be15928d45bf02eb
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-02-05 10:16:16 -0500

    Rename boilerplate for consistency and increment patch version number

--- sample/script.js
+++ sample/script.js
@@ -1,11 +1,12 @@
-import resolveRef from '../src/resolve-ref.js';
+import resolveRef from '../src/script.js';
 
 const refDictionary = {
 	keyA: true,
 	keyB: 123,
 	keyC: 'string',
 	keyD: { ref: 'keyA' },
-	keyE: { ref: 'keyD' }
+	keyE: { ref: 'keyD' },
+	keyF: { ref: 'keyE' }
 };
 
 console.log('keyA', resolveRef(refDictionary, 'keyA'));
@@ -13,3 +14,4 @@ console.log('keyB', resolveRef(refDictionary, 'keyB'));
 console.log('keyC', resolveRef(refDictionary, 'keyC'));
 console.log('keyD', resolveRef(refDictionary, 'keyD'));
 console.log('keyE', resolveRef(refDictionary, 'keyE'));
+console.log('keyF', resolveRef(refDictionary, 'keyF'));
